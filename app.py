462ad7f4b1bf7a6fa9ce3e4777e60ea87fae2299
lekidenye004 <josephkidenye@gmail.com>
2026-06-27 23:24:27 +0300

    Add email functionality using Flask-Mail

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'josephkidenye@gmail.com'          # sending address
-app.config['MAIL_PASSWORD'] = 'ykeq ggby iglc xzkq'    # use App Password, not regular password
+app.config['MAIL_PASSWORD'] = 'jguv rgxc nrlf chlr'    # use App Password, not regular password
 app.config['MAIL_DEFAULT_SENDER'] = 'josephkidenye@gmail.com'
 
 mail = Mail(app)
